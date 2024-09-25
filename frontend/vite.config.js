cfa2f775642081f3bfc8408a10917cc7b706c3f7
mgmanoj2018 <mgmanoj2018@gmail.com>
2024-09-25 07:03:36 +0530

    constant update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000 // Increase the chunk size limit to 1000 KB (default is 500 KB)
+  },
   plugins: [react()],
   resolve: {
     alias: {
